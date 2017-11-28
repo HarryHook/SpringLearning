3cdaed7566f346e236f0569e833d44b505d65612
HarryHook <chenhaowei93@163.com>
2017-11-28 18:11:28 +0800

    add 2000
    将jsp与数据库层分割，通过service进行和数据库的交互，判断model层中的user是否存在

---
+++ Spring_2000_Regisitration_2/src/com/bjsxt/registeration/model/User.java
@@ -0,0 +1,26 @@
+package com.bjsxt.registeration.model;
+
+/*
+    只有get*(), set*()方法的称为贫血模型
+    充血模型：还有其他的业务逻辑
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
